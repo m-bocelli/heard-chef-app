eae17389e1cf7722853b10d45f87a7d1e7f2f222
Mikey <michael.j.bocelli@gmail.com>
2024-10-15 22:58:11 -0400

    refactor: types cleaned up with extensions

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,25 +1,21 @@
-export const API_BASE_URL: string = process.env.EXPO_PUBLIC_API_URL!;
-
-export interface Recipe {
+export interface BaseEntity {
   id: number;
+  createdBy: string;
+  createdDate: string;
+  modifiedBy: string;
+  modifiedDate: string;
+}
+
+export interface Recipe extends BaseEntity {
   image1: string;
   title: string;
   netTime: number;
   rating: number;
   numReviews: number;
-  createdBy: string;
-  createdDate: string;
-  updatedBy: string;
-  updatedDate: string;
 }
 
-export interface User {
-  id: number;
+export interface User extends BaseEntity {
   username: string;
   herdId: string;
   userAccountId: number;
-  createdBy: string;
-  createdDate: string;
-  updatedBy: string;
-  updatedDate: string;
 }
